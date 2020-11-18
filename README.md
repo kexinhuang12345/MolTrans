5928d66b4c821269455d9a42a03373e916e02481
Kexin Huang <kexinhuang@hsph.harvard.edu>
2020-11-17 17:18:01 -0800

    update

--- README.md
+++ README.md
@@ -2,9 +2,15 @@
 
 Drug target interaction (DTI) prediction is a foundational task for in-silico drug discovery, which is costly and time-consuming due to the need of experimental search over large drug compound space. Recent years have witnessed promising progress for deep learning in DTI predictions. However, the following challenges are still open: (1) existing molecular representation learning approaches ignore the sub-structural nature of DTI, thus produce results that are less accurate and difficult to explain; (2) existing methods focus on limited labeled data while ignoring the value of massive unlabelled molecular data. We propose a Molecular Interaction Transformer (MolTrans) to address these limitations via: (1) knowledge inspired sub-structural pattern mining algorithm and interaction modeling module for more accurate and interpretable DTI prediction; (2) an augmented transformer encoder to better extract and capture the semantic relations among substructures extracted from massive unlabeled biomedical data. We evaluate MolTrans on real world data and show it improved DTI prediction performance compared to state-of-the-art baselines.
 
-## Run
-```
-python train.py
-```
 
-More details will be released in a week or so.
+## Datasets
+
+In the dataset folder, we provide all three processed datasets used in MolTrans: BindingDB, DAVIS, and BIOSNAP. In BIOSNAP folder, there is full dataset for the main experiment, and also missing data experiment (70%, 80%, 90%, 95%) and unseen drug and unseen protein datasets.
+
+## Run
+
+We provide an example jupyter notebook in the repository. Although it runs for 100 epochs, we find 50 epochs is way enough and all the results in paper are run by 50 epochs. 
+
+You can also directly run moltrans_X.py for each dataset.
+
+Will add more codes and tests in the next couple of weeks. But this should be enough to try on MolTrans.
---
+++ moltrans_biosnap.py
@@ -0,0 +1,180 @@
+import torch
+from torch.autograd import Variable
+import torch.nn.functional as F
+from torch.utils import data
+from torch import nn 
+import copy
+
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from time import time
+from sklearn.metrics import roc_auc_score, average_precision_score, f1_score, roc_curve, confusion_matrix, precision_score, recall_score, auc
+from sklearn.model_selection import KFold
+torch.manual_seed(1)    # reproducible torch:2 np:3
+np.random.seed(1)
+
+from config import BIN_config_DBPE
+from models import BIN_Interaction_Flat
+from stream import BIN_Data_Encoder
+
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda:0" if use_cuda else "cpu")
+
+def test(data_generator, model):
+    y_pred = []
+    y_label = []
+    model.eval()
+    loss_accumulate = 0.0
+    count = 0.0
+    for i, (d, p, d_mask, p_mask, label) in enumerate(data_generator):
+        score = model(d.long().cuda(), p.long().cuda(), d_mask.long().cuda(), p_mask.long().cuda())
+        
+        m = torch.nn.Sigmoid()
+        logits = torch.squeeze(m(score))
+        loss_fct = torch.nn.BCELoss()            
+        
+        label = Variable(torch.from_numpy(np.array(label)).float()).cuda()
+
+        loss = loss_fct(logits, label)
+        
+        loss_accumulate += loss
+        count += 1
+        
+        logits = logits.detach().cpu().numpy()
+        
+        label_ids = label.to('cpu').numpy()
+        y_label = y_label + label_ids.flatten().tolist()
+        y_pred = y_pred + logits.flatten().tolist()
+        
+    loss = loss_accumulate/count
+    
+    fpr, tpr, thresholds = roc_curve(y_label, y_pred)
+
+    precision = tpr / (tpr + fpr)
+
+    f1 = 2 * precision * tpr / (tpr + precision + 0.00001)
+
+    thred_optim = thresholds[5:][np.argmax(f1[5:])]
+
+    print("optimal threshold: " + str(thred_optim))
+
+    y_pred_s = [1 if i else 0 for i in (y_pred >= thred_optim)]
+
+    auc_k = auc(fpr, tpr)
+    print("AUROC:" + str(auc_k))
+    print("AUPRC: "+ str(average_precision_score(y_label, y_pred)))
+
+    cm1 = confusion_matrix(y_label, y_pred_s)
+    print('Confusion Matrix : \n', cm1)
+    print('Recall : ', recall_score(y_label, y_pred_s))
+    print('Precision : ', precision_score(y_label, y_pred_s))
+
+    total1=sum(sum(cm1))
+    #####from confusion matrix calculate accuracy
+    accuracy1=(cm1[0,0]+cm1[1,1])/total1
+    print ('Accuracy : ', accuracy1)
+
+    sensitivity1 = cm1[0,0]/(cm1[0,0]+cm1[0,1])
+    print('Sensitivity : ', sensitivity1 )
+
+    specificity1 = cm1[1,1]/(cm1[1,0]+cm1[1,1])
+    print('Specificity : ', specificity1)
+
+    outputs = np.asarray([1 if i else 0 for i in (np.asarray(y_pred) >= 0.5)])
+    return roc_auc_score(y_label, y_pred), average_precision_score(y_label, y_pred), f1_score(y_label, outputs), y_pred, loss.item()
+
+
+def main(fold_n, lr):
+    config = BIN_config_DBPE()
+    
+    lr = lr
+    BATCH_SIZE = config['batch_size']
+    train_epoch = 50
+    
+    loss_history = []
+    
+    model = BIN_Interaction_Flat(**config)
+    
+    model = model.cuda()
+
+    if torch.cuda.device_count() > 1:
+        print("Let's use", torch.cuda.device_count(), "GPUs!")
+        model = nn.DataParallel(model, dim = 0)
+            
+    opt = torch.optim.Adam(model.parameters(), lr = lr)
+    
+    print('--- Data Preparation ---')
+    
+    params = {'batch_size': BATCH_SIZE,
+              'shuffle': True,
+              'num_workers': 6, 
+              'drop_last': True}
+
+    dataFolder = './dataset/BIOSNAP/full_data'
+
+    df_train = pd.read_csv(dataFolder + '/train.csv')
+    df_val = pd.read_csv(dataFolder + '/val.csv')
+    df_test = pd.read_csv(dataFolder + '/test.csv')
+    
+    training_set = BIN_Data_Encoder(df_train.index.values, df_train.Label.values, df_train)
+    training_generator = data.DataLoader(training_set, **params)
+
+    validation_set = BIN_Data_Encoder(df_val.index.values, df_val.Label.values, df_val)
+    validation_generator = data.DataLoader(validation_set, **params)
+    
+    testing_set = BIN_Data_Encoder(df_test.index.values, df_test.Label.values, df_test)
+    testing_generator = data.DataLoader(testing_set, **params)
+    
+    # early stopping
+    max_auc = 0
+    model_max = copy.deepcopy(model)
+    
+    with torch.set_grad_enabled(False):
+        auc, auprc, f1, logits, loss = test(testing_generator, model_max)
+        print('Initial Testing AUROC: ' + str(auc) + ' , AUPRC: ' + str(auprc) + ' , F1: '+str(f1) + ' , Test loss: '+str(loss))
+    
+    print('--- Go for Training ---')
+    torch.backends.cudnn.benchmark = True
+    for epo in range(train_epoch):
+        model.train()
+        for i, (d, p, d_mask, p_mask, label) in enumerate(training_generator):
+            score = model(d.long().cuda(), p.long().cuda(), d_mask.long().cuda(), p_mask.long().cuda())
+
+            label = Variable(torch.from_numpy(np.array(label)).float()).cuda()
+            
+            loss_fct = torch.nn.BCELoss()
+            m = torch.nn.Sigmoid()
+            n = torch.squeeze(m(score))
+            
+            loss = loss_fct(n, label)
+            loss_history.append(loss)
+            
+            opt.zero_grad()
+            loss.backward()
+            opt.step()
+            
+            if (i % 1000 == 0):
+                print('Training at Epoch ' + str(epo + 1) + ' iteration ' + str(i) + ' with loss ' + str(loss.cpu().detach().numpy()))
+            
+        with torch.set_grad_enabled(False):
+            auc, auprc, f1, logits, loss = test(validation_generator, model)
+            if auc > max_auc:
+                model_max = copy.deepcopy(model)
+                max_auc = auc
+            print('Validation at Epoch '+ str(epo + 1) + ' , AUROC: '+ str(auc) + ' , AUPRC: ' + str(auprc) + ' , F1: '+str(f1))
+    
+    print('--- Go for Testing ---')
+    try:
+        with torch.set_grad_enabled(False):
+            auc, auprc, f1, logits, loss = test(testing_generator, model_max)
+            print('Testing AUROC: ' + str(auc) + ' , AUPRC: ' + str(auprc) + ' , F1: '+str(f1) + ' , Test loss: '+str(loss))
+    except:
+        print('testing failed')
+    return model_max, loss_history
+
+s = time()
+model_max, loss_history = main(5e-6)
+e = time()
+print(e-s)
