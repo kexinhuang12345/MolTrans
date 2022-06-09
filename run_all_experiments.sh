183d39981e7e1b227ce0c3669f6271b0a88373da
Ken <po.yu.kao@gmail.com>
2022-06-09 09:03:50 +0000

    changed gpu #1

--- run_all_experiments.sh
+++ run_all_experiments.sh
@@ -5,7 +5,7 @@ for d in 'biosnap' 'bindingdb' 'davis' ;
 do
     for i in 1 2 3 4 5 ;
     do
-        echo "CUDA_VISIBLE_DEVICES=0,2,5,6 python train.py --task $d >> logs/log_${d}_${i}.txt"
-        CUDA_VISIBLE_DEVICES=0,2,5,6 python train.py --task $d >> logs/log_${d}_${i}.txt
+        echo "CUDA_VISIBLE_DEVICES=3,5,6,7 python train.py --task $d >> logs/log_${d}_${i}.txt"
+        CUDA_VISIBLE_DEVICES=3,5,6,7 python train.py --task $d >> logs/log_${d}_${i}.txt
     done
 done
